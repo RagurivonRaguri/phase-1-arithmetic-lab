8a31a292c9efb14bc94efba20ffe8acd9240d8d3
LincolnMuraguri <lincolnmuraguri93@gmail.com>
2023-03-15 11:14:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
-// Write your code here
+let num1=2
+let num2=31
+let multiply=(num1*num2)
+console.log(multiply)
+let random=Math.round(Math.random())+1;
+let num3=18
+let num4=7
+const mod=(num3%num4)
+console.log(mod)
+let max=Math.max(4,5,20);
